fff9ea6b9169f38687ffbd955ceb6b1091eacc82
sgatana <sgatana@gmail.com>
2017-09-20 14:38:55 +0300

    update to follow pep-8 standards

--- tests/user_test.py
+++ tests/user_test.py
@@ -7,29 +7,39 @@ from cart_models.user import User
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-        #Create global User And ShoppingList for use throughout the class
+        """Create global User And ShoppingList for use throughout the class"""
         self.user = User("gatana", "test@gmail.com", "test@2017")
         self.shopping_list = ShoppingList("Naivas", "Shop with a difference")
 
-        #Ensure user can create a shopping list
+        """Ensure user can create a shopping list"""
     def test_user_can_create_shopping_list(self):
         self.user.create_shopping_lst(self.shopping_list)
         self.assertEqual(1, len(self.user.shopping_lists))
 
-        #Ensure user can delete a shopping list
+        """Ensure user can delete a shopping list"""
     def test_user_can_delete_a_shopping_list(self):
         self.user.create_shopping_lst(self.shopping_list)
         self.assertEqual(1, len(self.user.shopping_lists))
         self.user.delete_shopping_list(self.shopping_list.name)
         self.assertEqual(0, len(self.user.shopping_lists))
 
-    
+    def test_user_delete_unexisting_shopping_list(self):
+        self.assertEqual(0, len(self.user.shopping_lists))
+        with self.assertRaises(ShoppingListDoesNotExist):
+            self.user.delete_shopping_list("List does not exist")
+
+    def test_exception_raised_on_try_to_create_similar_shopping_lists(self):
+        self.user.create_shopping_lst(self.shopping_list)
+        self.user.create_shopping_lst(ShoppingList("added to list"))
+        with self.assertRaises(ShoppingListAlreadyExist):
+            self.user.create_shopping_lst(ShoppingList("Already existing"))
 
     def test_get_shopping_list_returns_if_item_found(self):
-        """
-        tests if the get.. method in user returns the shoppingList Specified by
+
+        """tests if the get.. method in user returns the shoppingList Specified by
         :key and raises (ShoppingListDoesNotExist)exception if not found
         """
+
         self.user.create_shopping_lst(self.shopping_list)
         self.assertEqual(1, len(self.user.shopping_lists))
         self.assertIsInstance(self.user.get_shopping_lst("Naivas"), ShoppingList)
