25e9805671827f3456b0932ab2e2782e5f9008c5
sgatana <sgatana@gmail.com>
2017-09-20 17:33:32 +0300

    Add Exceptions test for users

---
+++ Exceptions.py
@@ -0,0 +1,9 @@
+
+class UserAlreadyExist(Exception):
+    """If a User Does Exist This Exception is thrown"""
+    pass
+
+
+class UserDoesNotExist(Exception):
+    """If a User Does Not Exist This Exception is thrown"""
+    pass
