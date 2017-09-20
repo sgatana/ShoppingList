d8b478a34fc616ef54e9366cdb96ffdc61082d9f
sgatana <sgatana@gmail.com>
2017-09-20 17:49:47 +0300

    [chore] update exception file

--- Exceptions.py
+++ Exceptions.py
@@ -1,3 +1,12 @@
+class ItemDoesNotExist(Exception):
+    """If an Item Does Not Exist This Exception is thrown"""
+    pass
+
+
+class ItemAlreadyExist(Exception):
+    """If an Item Does Exist This Exception is thrown"""
+    pass
+
 
 class UserAlreadyExist(Exception):
     """If a User Does Exist This Exception is thrown"""
@@ -6,4 +15,15 @@ class UserAlreadyExist(Exception):
 
 class UserDoesNotExist(Exception):
     """If a User Does Not Exist This Exception is thrown"""
+    pass
+
+
+class ShoppingListDoesNotExist(Exception):
+    """Raised If A non-existing ShoppingList removed from the shopping List
+    """
+    pass
+
+
+class ShoppingListAlreadyExist(Exception):
+    """Raised on Attempt to add an existing shopping list """
     pass
