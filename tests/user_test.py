94399771f10b31412df329c1d3a66c0b06a70aa4
sgatana <sgatana@gmail.com>
2017-09-20 13:58:51 +0300

    add unit tests

---
+++ tests/user_test.py
@@ -0,0 +1,44 @@
+import unittest
+
+from Exceptions import ShoppingListDoesNotExist, ShoppingListAlreadyExist
+from cart_models.Shopping_list import ShoppingList
+from cart_models.user import User
+
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        #Create global User And ShoppingList for use throughout the class
+        self.user = User("gatana", "test@gmail.com", "test@2017")
+        self.shopping_list = ShoppingList("Naivas", "Shop with a difference")
+
+        #Ensure user can create a shopping list
+    def test_user_can_create_shopping_list(self):
+        self.user.create_shopping_lst(self.shopping_list)
+        self.assertEqual(1, len(self.user.shopping_lists))
+
+        #Ensure user can delete a shopping list
+    def test_user_can_delete_a_shopping_list(self):
+        self.user.create_shopping_lst(self.shopping_list)
+        self.assertEqual(1, len(self.user.shopping_lists))
+        self.user.delete_shopping_list(self.shopping_list.name)
+        self.assertEqual(0, len(self.user.shopping_lists))
+
+    
+
+    def test_get_shopping_list_returns_if_item_found(self):
+        """
+        tests if the get.. method in user returns the shoppingList Specified by
+        :key and raises (ShoppingListDoesNotExist)exception if not found
+        """
+        self.user.create_shopping_lst(self.shopping_list)
+        self.assertEqual(1, len(self.user.shopping_lists))
+        self.assertIsInstance(self.user.get_shopping_lst("Naivas"), ShoppingList)
+        self.assertEqual("Naivas", self.user.get_shopping_lst("Naivas").name)
+        self.assertEqual(1, len(self.user.shopping_lists))
+
+    def test_get_shoppinglist_raises_exception_if_shoppinglist_not_found(self):
+        with self.assertRaises(ShoppingListDoesNotExist):
+            self.user.get_shopping_lst("Shopping List does not exist")
+
+if __name__ == '__main__':
+    unittest.main()
